c85cdcabd35036ba6a5ace6f854f6a4e44a4676b
serge <sergey.baichyk@yandex.ru>
2022-11-22 01:14:16 +0300

    add exception

---
+++ src/main/java/com/bets/betsproject/exception/ResourceNotFoundException.java
@@ -0,0 +1,60 @@
+package com.bets.betsproject.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+    private String resourceName;
+    private String fieldName;
+    private Object filedValue;
+
+    public ResourceNotFoundException(String resourceName, String fieldName, Object filedValue) {
+        super(String.format("%s not found with %s : '%s'", resourceName, fieldName, filedValue));
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+        this.filedValue = filedValue;
+    }
+
+    public ResourceNotFoundException(String message, String resourceName, String fieldName, Object filedValue) {
+        super(message);
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+        this.filedValue = filedValue;
+    }
+
+    public ResourceNotFoundException(String message, Throwable cause, String resourceName, String fieldName, Object filedValue) {
+        super(message, cause);
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+        this.filedValue = filedValue;
+    }
+
+    public ResourceNotFoundException(Throwable cause, String resourceName, String fieldName, Object filedValue) {
+        super(cause);
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+        this.filedValue = filedValue;
+    }
+
+    public ResourceNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace, String resourceName, String fieldName, Object filedValue) {
+        super(message, cause, enableSuppression, writableStackTrace);
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+        this.filedValue = filedValue;
+    }
+
+    public String getResourceName() {
+        return resourceName;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public Object getFiledValue() {
+        return filedValue;
+    }
+
+
+}
