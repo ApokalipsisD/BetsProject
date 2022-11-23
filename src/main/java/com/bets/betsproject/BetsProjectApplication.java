c554466360342ca53daa8ce92b23f1786214fb89
serge <sergey.baichyk@yandex.ru>
2022-11-23 14:36:58 +0300

    add main

---
+++ src/main/java/com/bets/betsproject/BetsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.bets.betsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetsProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BetsProjectApplication.class, args);
+    }
+
+}
